1ef403c93b155f0a9534d2b021c3732330900326
JavierMedina92 <javiermedina0192@gmail.com>
2024-03-10 17:05:07 -0700

    Se agregan estilos y afinan detales a Sobre Mi

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-// function mostrarParrafo() {
-//     var parrafo = document.getElementById("parrafoOculto");
-//     if (parrafo.style.display === "none") {
-//         parrafo.style.display = "block";
-//     } else {
-//         parrafo.style.display = "none";
-//     }
-// }
+function mostrarParrafo() {
+    var parrafo = document.getElementById("parrafoOculto");
+    if (parrafo.style.display === "none") {
+        parrafo.style.display = "block";
+    } else {
+        parrafo.style.display = "none";
+    }
+}
