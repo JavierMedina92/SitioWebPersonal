8882bd88ef50e07add22e64323998b6f531913b7
JavierMedina92 <javiermedina0192@gmail.com>
2024-03-11 12:24:07 -0700

    agregando detalles a Formulario y antes de agregar funcionalidad

--- js/app.js
+++
@@ -1,8 +0,0 @@
-function mostrarParrafo() {
-    var parrafo = document.getElementById("parrafoOculto");
-    if (parrafo.style.display === "none") {
-        parrafo.style.display = "block";
-    } else {
-        parrafo.style.display = "none";
-    }
-}
