079d5ee88d0793e09227983d63a30d8bc9f20b2b
JavierMedina92 <javiermedina0192@gmail.com>
2024-03-14 12:22:31 -0700

    Se agregan ultimos detalles Esteticos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 
 document.getElementById('agradecimientoBtn').addEventListener('click', () => {
-    alert('Gracias por llegar hasta aquí. Algunos proyectos aún están en desarrollo.');
+    alert('Gracias por llegar hasta aquí. Algunos proyectos aún están en desarrollo pero en cuanto los termine los subire de inmediato.');
 });
 
+//Pendiente agregar mas funcionalidade
